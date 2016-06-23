cfd177efaa89d8e3136c50477b30e0b3f53af154
parp1 <parth.pendurkar@gmail.com>
2016-06-23 13:34:54 -0700

    JSON implemented

--- src/JSONInspector.java
+++ src/JSONInspector.java
@@ -1,5 +1,7 @@
 import com.thoughtworks.qdox.model.JavaAnnotation;
 import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaParameterizedType;
+import com.thoughtworks.qdox.model.impl.DefaultJavaParameterizedType;
 import org.json.simple.JSONObject;
 
 import java.util.ArrayList;
@@ -20,17 +22,28 @@ public class JSONInspector
 
     public String toString(String className)
     {
-        JavaClass c = (JavaClass) jsonObject.get(className);
+        //JavaClass c = (JavaClass) jsonObject.get(className);
         boolean hSA = (boolean) jsonObject.get(className + " has service");
         boolean hCA = (boolean) jsonObject.get(className + " has component");
         List<JavaAnnotation> jca = (LinkedList) jsonObject.get(className + " class annotations");
-        List<JavaAnnotation> jas = (ArrayList) jsonObject.get(className + " annotations");
+        List<JavaAnnotation> jac = (ArrayList) jsonObject.get(className + " annotations");
+        List<JavaClass> jic = (List) jsonObject.get(className + " implemented classes");
+
+        System.out.println("JSON toString for " + className + ".");
+        System.out.println("");
 
         System.out.println(className + " has service annotations: " + hSA);
         System.out.println(className + " has component annotations: " + hCA);
         System.out.println(className + " has " + jca.size() + " class annotations recovered from the JSON");
-        System.out.println(className + " has " + jas.size() + " compiled annotations recovered from the JSON");
+        System.out.println(className + " has " + jac.size() + " compiled annotations recovered from the JSON");
 
+        if (jic.size() > 0)
+        {
+            System.out.println(className + " implements all of the classes below");
+            jic.forEach(jc -> System.out.println("-" + jc.getName()));
+        }
+
+        System.out.println("");
         return "JSON toString for " + className + " completed.";
     }
 
